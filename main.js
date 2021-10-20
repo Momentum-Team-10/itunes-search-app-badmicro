3bb2d56e69ddf4a9f8e0f2e3c82af1e1f906fae3
Jeffrey <jrgilley92@gmail.com>
2021-10-20 12:39:43 -0400

    Working fetch! Working display

--- main.js
+++ main.js
@@ -1,33 +1,56 @@
 /* globals fetch, moment */
 
-const url = "https://itunes.apple.com/search?";
+//const url = "https://proxy-itunes-api.glitch.me/";
+const url = "https://proxy-itunes-api.glitch.me/search?media=music&entity=song&term="
+const songsList = document.getElementById("song-list");
 const form = document.querySelector("#song-form");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const searchInput = document.getElementById("search-param");
 
-    fetch(generateUrl(searchInput.value))
+    fetch(url + searchInput.value)
         .then(res => res.json())
         .then(data => {
+            //for every item create a list element that is
+            // img, song title, band
             console.log(data)
+            for(let item of data.results) {
+                if(item.kind === 'song'){
+                    generateSongCard(item)
+                }
+            }
         })
 })
 
 /*
-    Generates a url that itunes accepts bases on the input string
-    Note: Working as intended!
+    Generates a Song "Card" to add to the UL of playable options
+    for the user to choose from to play a song sample
+    extra note: "create evenlistener here?"
 */
 
-function generateUrl(input) {
-    let terms = input.toString().trim().replace(' ', '+')
-    return url + "term=" + terms
+function generateSongCard(songObj) {
+    if(songObj.kind === "song") {
+        const li = document.createElement('li');
+        li.trackId = songObj.trackId;
+        li.imgSrc = songObj.artworkUrl100;
+        li.trackName = songObj.trackName;
+        li.artistName = songObj.artistName;
+        //add styles to song card here with li.classList.add();
+
+        renderSongCard(li);
+        songsList.appendChild(li);
+    } else {
+        console.error("Invalid Filetype, cannot generate song card");
+    }
 }
 
-/*
-
-*/
-
-function renderList() {
-
+function renderSongCard(li) {
+    li.innerHTML = `
+    <div class="Song-Card">
+        <img src="${li.imgSrc}" alt="Album Cover">
+        <p>${li.trackName}</p>
+        <p>${li.artistName}</p>
+    </div>
+    `
 }
