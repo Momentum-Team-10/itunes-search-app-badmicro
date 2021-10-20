98d11d7dbd411cc74b623650bce0fb0180129139
Jeffrey <jrgilley92@gmail.com>
2021-10-20 16:43:22 -0400

    Working player! Auto-plays music choice

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
 /* globals fetch, moment */
 
-//const url = "https://proxy-itunes-api.glitch.me/";
-const url = "https://proxy-itunes-api.glitch.me/search?media=music&entity=song&term="
+const url = "https://itunes.apple.com/search?term="
 const songsList = document.getElementById("song-list");
 const form = document.querySelector("#song-form");
+const audioPlayer = document.getElementById("audio-player");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const searchInput = document.getElementById("search-param");
+  e.preventDefault();
+  const searchInput = document.getElementById("search-param");
 
-    fetch(url + searchInput.value)
-        .then(res => res.json())
-        .then(data => {
-            //for every item create a list element that is
-            // img, song title, band
-            console.log(data)
-            for(let item of data.results) {
-                if(item.kind === 'song'){
-                    generateSongCard(item)
-                }
-            }
-        })
+  fetch(url + searchInput.value)
+    .then((res) => res.json())
+    .then((data) => {
+      //for every item create a list element that is
+      // img, song title, band
+      //console.log(data);
+      for (let item of data.results) {
+        if (item.kind === "song") {
+          generateSongCard(item);
+        }
+      }
+    });
+});
+
+songsList.addEventListener("click", (e) => {
+  console.log(e.target.id)
+  if(e.target.id) {
+    audioPlayer.src = e.target.id;
+    audioPlayer.play();
+  }
 })
+
 
 /*
     Generates a Song "Card" to add to the UL of playable options
@@ -30,27 +39,28 @@ form.addEventListener("submit", (e) => {
 */
 
 function generateSongCard(songObj) {
-    if(songObj.kind === "song") {
-        const li = document.createElement('li');
-        li.trackId = songObj.trackId;
-        li.imgSrc = songObj.artworkUrl100;
-        li.trackName = songObj.trackName;
-        li.artistName = songObj.artistName;
-        //add styles to song card here with li.classList.add();
+  if (songObj.kind === "song") {
+    const li = document.createElement("li");
+    //li.trackId = songObj.trackId;
+    li.imgSrc = songObj.artworkUrl100;
+    li.trackName = songObj.trackName;
+    li.artistName = songObj.artistName;
+    li.musicSrc = songObj.previewUrl;
+    //add styles to song card here with li.classList.add();
 
-        renderSongCard(li);
-        songsList.appendChild(li);
-    } else {
-        console.error("Invalid Filetype, cannot generate song card");
-    }
+    renderSongCard(li);
+    songsList.appendChild(li);
+  } else {
+    console.error("Invalid Filetype, cannot generate song card");
+  }
 }
 
 function renderSongCard(li) {
-    li.innerHTML = `
+  li.innerHTML = `
     <div class="Song-Card">
         <img src="${li.imgSrc}" alt="Album Cover">
-        <p>${li.trackName}</p>
+        <p id=${li.musicSrc}>${li.trackName}</p>
         <p>${li.artistName}</p>
     </div>
-    `
+    `;
 }
